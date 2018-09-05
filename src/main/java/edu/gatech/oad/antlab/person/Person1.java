c29c543035a24568d4239469178954dbec4788a8
rahul <rahul199722@gmail.com>
2018-09-05 16:55:31 -0400

    Covered the edge case for person 1 implementation

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,6 +31,9 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
+		if (input.length() < 3) {
+			return input;
+		}
 	  return input.subString(2) + input.subString(0, 2);
 	}
 	
