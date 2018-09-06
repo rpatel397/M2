30849616f4822ac3b87494417a484bfc35e72faf
Rakeshgore <42789046+Rakeshgore@users.noreply.github.com>
2018-09-05 20:49:03 -0400

    Added code for P.2

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -30,8 +30,16 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+        String randomString = "";
+        while (input.length() != 0)
+        {
+            int index = (int) Math.floor(Math.random() * input.length());
+            char c = input.charAt(index);
+            input = input.substring(0,index)+ input.substring(index+1);
+            randomString += c;
+        }
+
+        return randomString;
 	}
 	/**
 	 * Return a string rep of this object
